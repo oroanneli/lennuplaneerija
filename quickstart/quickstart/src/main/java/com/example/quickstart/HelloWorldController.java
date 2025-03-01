a7eeb6f0194b2b82e400dce1fe3393d63ec653db
anneli-oro <annelioro@ut.ee>
2025-03-01 22:57:36 +0200

    veebileht mis ütleb mulle tere maailm, suht edusammud

--- quickstart/quickstart/src/main/java/com/example/quickstart/HelloWorldController.java
+++ quickstart/quickstart/src/main/java/com/example/quickstart/HelloWorldController.java
@@ -1,5 +1,15 @@
 package com.example.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
-    
+
+    @GetMapping(path ="/hello")
+    public String helloWorld(){
+        return "Tere Maailm!";
+
+    }
+
 }

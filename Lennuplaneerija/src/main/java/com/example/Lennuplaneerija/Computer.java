0fba58bf6e726e40cad72d5785ba2b5bb095025b
anneli-oro <annelioro@ut.ee>
2025-03-03 19:23:55 +0200

    teen hästi palju projekte

--- Lennuplaneerija/src/main/java/com/example/Lennuplaneerija/Computer.java
+++
@@ -1,5 +0,0 @@
-package com.example.Lennuplaneerija;
-
-public interface Computer {
-    void compile();
-}
--- Lennuplaneerija/src/main/java/com/example/Lennuplaneerija/LennuplaneerijaApplication.java
+++ Lennuplaneerija/src/main/java/com/example/Lennuplaneerija/LennuplaneerijaApplication.java
@@ -1,19 +1,13 @@
 package com.example.Lennuplaneerija;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LennuplaneerijaApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(LennuplaneerijaApplication.class, args);
-
-		Dev obj = context.getBean(Dev.class);
-		obj.build();
+		SpringApplication.run(LennuplaneerijaApplication.class, args);
 	}
-
 
 }
